eb98033ece85cfd1a5fe752a06a81e843db49963
PANKAJ KUMAR <pkd.234@gmail.com>
2018-05-28 08:54:18 +0530

    Initial commit

---
+++ superlibrary/src/main/java/com/example/superlibrary/Quick.java
@@ -0,0 +1,10 @@
+package com.example.superlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Quick {
+    public static void ToastQuick(Context context, String dd){
+        Toast.makeText(context, dd, Toast.LENGTH_SHORT).show();
+    }
+}
