2ee5910c9394696c196485154ac3972e674e2c2e
PANKAJ KUMAR <pkd.234@gmail.com>
2018-05-28 18:31:26 +0530

    third
    third

--- superlibrary/src/main/java/com/example/superlibrary/Quick.java
+++ superlibrary/src/main/java/com/example/superlibrary/Quick.java
@@ -7,4 +7,7 @@ public class Quick {
     public static void ToastQuick(Context context, String dd){
         Toast.makeText(context, dd, Toast.LENGTH_SHORT).show();
     }
+    public static void ToastQuickLong(Context context, String dd){
+        Toast.makeText(context, dd, Toast.LENGTH_LONG).show();
+    }
 }
